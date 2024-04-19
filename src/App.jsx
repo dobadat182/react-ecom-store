ed72b7e2e6aac1b6d9a386375f107bb6a5e6339c
Coder <coder@Brians-iMac.local>
2024-04-19 16:15:49 +0700

    Add react-lucide/file-structured

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Routers from './Routers/Routers';
 function App() {
     return (
         <div>
-            <h1>Konnect with Ahsan</h1>
-
             <Routers />
         </div>
     );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 const index = () => {
-  return <div>Home Page</div>;
+    return <div>Home Page</div>;
 };
 
 export default index;
