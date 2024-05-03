45764b97dad6384c9629ec16043feca14d5916c5
Brian Do <badat336@gmail.com>
2024-05-03 16:22:25 +0700

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
-export { default as Home } from './home';
-export { default as Login } from './login';
-export { default as Products } from './products';
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Products } from './Products';
